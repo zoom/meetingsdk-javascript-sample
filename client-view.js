2ac3b778dcd2a18a6fe7286c4966fce50085e0f3
tommygaessler <tommy.gaessler@zoom.us>
2022-06-03 10:58:31 -0600

    upgraded to meeting sdk 2.4.5

--- client-view.js
+++ client-view.js
@@ -1,24 +1,24 @@
-ZoomMtg.preLoadWasm();
-ZoomMtg.prepareWebSDK();
+ZoomMtg.preLoadWasm()
+ZoomMtg.prepareWebSDK()
 // loads language files, also passes any error messages to the ui
-ZoomMtg.i18n.load('en-US');
-ZoomMtg.i18n.reload('en-US');
+ZoomMtg.i18n.load('en-US')
+ZoomMtg.i18n.reload('en-US')
 
-ZoomMtg.setZoomJSLib('https://source.zoom.us/2.4.0/lib', '/av');
+ZoomMtg.setZoomJSLib('https://source.zoom.us/2.4.5/lib', '/av')
 
 // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
-var signatureEndpoint = '';
-var sdkKey = '';
-var meetingNumber = '123456789';
-var role = 0;
-var leaveUrl = 'https://zoom.us';
-var userName = 'JavaScript';
+var signatureEndpoint = ''
+var sdkKey = ''
+var meetingNumber = '123456789'
+var role = 0
+var leaveUrl = 'http://localhost:8080'
+var userName = 'JavaScript'
 var userEmail = ''
-var passWord = '';
+var passWord = ''
 // pass in the registrant's token if your meeting or webinar requires registration. More info here:
-// Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-registered
-// Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-registered
-var registrantToken = '';
+// Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-meeting-with-registration-required
+// Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-webinar-with-registration-required
+var registrantToken = ''
 
 function getSignature() {
   fetch(signatureEndpoint, {
@@ -31,13 +31,13 @@ function getSignature() {
       role: role
     })
   }).then((response) => {
-    return response.json();
+    return response.json()
   }).then((data) => {
     console.log(data)
     startMeeting(data.signature)
   }).catch((error) => {
-  	console.log(error);
-  });
+  	console.log(error)
+  })
 }
 
 function startMeeting(signature) {
@@ -57,12 +57,12 @@ function startMeeting(signature) {
         passWord: passWord,
         tk: registrantToken,
         success: (success) => {
-          console.log(success);
+          console.log(success)
         },
         error: (error) => {
-          console.log(error);
+          console.log(error)
         },
-      });
+      })
     },
     error: (error) => {
       console.log(error)
--- component-view.js
+++ component-view.js
@@ -1,24 +1,24 @@
-const client = ZoomMtgEmbedded.createClient();
+const client = ZoomMtgEmbedded.createClient()
 
-let meetingSDKElement = document.getElementById('meetingSDKElement');
+let meetingSDKElement = document.getElementById('meetingSDKElement')
 
 // setup your signature endpoint here: https://github.com/zoom/meetingsdk-sample-signature-node.js
-var signatureEndpoint = '';
-var sdkKey = '';
-var meetingNumber = '123456789';
-var role = 0;
-var userName = 'JavaScript';
+var signatureEndpoint = ''
+var sdkKey = ''
+var meetingNumber = '123456789'
+var role = 0
+var userName = 'JavaScript'
 var userEmail = ''
-var passWord = '';
+var passWord = ''
 // pass in the registrant's token if your meeting or webinar requires registration. More info here:
-// Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/meetings#join-registered
-// Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/client-view/webinars#join-registered
-var registrantToken = '';
+// Meetings: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/meetings#join-meeting-with-registration-required
+// Webinars: https://marketplace.zoom.us/docs/sdk/native-sdks/web/component-view/webinars#join-webinar-with-registration-required
+var registrantToken = ''
 
 client.init({
   zoomAppRoot: meetingSDKElement,
   language: 'en-US',
-});
+})
 
 function getSignature() {
   fetch(signatureEndpoint, {
@@ -31,13 +31,13 @@ function getSignature() {
       role: role
     })
   }).then((response) => {
-    return response.json();
+    return response.json()
   }).then((data) => {
     console.log(data)
     startMeeting(data.signature)
   }).catch((error) => {
-  	console.log(error);
-  });
+  	console.log(error)
+  })
 }
 
 function startMeeting(signature) {
