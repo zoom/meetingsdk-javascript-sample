f45e5d401faab7ac49fbfc542972ca59941a39f8
Jack Yang <jack.yang@zoom.us>
2025-07-15 11:18:12 -0700

    update 4.0.0

--- client-view.js
+++ client-view.js
@@ -1,50 +1,50 @@
-ZoomMtg.preLoadWasm()
-ZoomMtg.prepareWebSDK()
+ZoomMtg.preLoadWasm();
+ZoomMtg.prepareWebSDK();
 
-var authEndpoint = ''
-var sdkKey = ''
-var meetingNumber = '123456789'
-var passWord = ''
-var role = 0
-var userName = 'JavaScript'
-var userEmail = ''
-var registrantToken = ''
-var zakToken = ''
-var leaveUrl = 'https://zoom.us'
+var authEndpoint = "";
+var meetingNumber = "123456789";
+var passWord = "";
+var role = 0;
+var userName = "JavaScript";
+var userEmail = "";
+var registrantToken = "";
+var zakToken = "";
+var leaveUrl = "https://zoom.us";
 
 function getSignature() {
   fetch(authEndpoint, {
-    method: 'POST',
+    method: "POST",
     headers: {
-      'Content-Type': 'application/json'
+      "Content-Type": "application/json",
     },
     body: JSON.stringify({
       meetingNumber: meetingNumber,
-      role: role
-    })
-  }).then((response) => {
-    return response.json()
-  }).then((data) => {
-    console.log(data)
-    startMeeting(data.signature)
-  }).catch((error) => {
-  	console.log(error)
+      role: role,
+    }),
   })
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      startMeeting(data.signature);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
 }
 
 function startMeeting(signature) {
-
-  document.getElementById('zmmtg-root').style.display = 'block'
+  document.getElementById("zmmtg-root").style.display = "block";
 
   ZoomMtg.init({
     leaveUrl: leaveUrl,
     patchJsMedia: true,
     leaveOnPageUnload: true,
     success: (success) => {
-      console.log(success)
+      console.log(success);
       ZoomMtg.join({
         signature: signature,
-        sdkKey: sdkKey,
         meetingNumber: meetingNumber,
         passWord: passWord,
         userName: userName,
@@ -52,15 +52,15 @@ function startMeeting(signature) {
         tk: registrantToken,
         zak: zakToken,
         success: (success) => {
-          console.log(success)
+          console.log(success);
         },
         error: (error) => {
-          console.log(error)
+          console.log(error);
         },
-      })
+      });
     },
     error: (error) => {
-      console.log(error)
-    }
-  })
+      console.log(error);
+    },
+  });
 }
